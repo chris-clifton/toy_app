092884e74a900ed76ec17bf6149ac1e6c99e3956
chris-clifton <cliftoncpx@gmail.com>
2019-01-10 16:18:12 -0500

    added scafford for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# the resources :microposts and resources :users routing rules map respective URLS to actions in micropost/users controller
+
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   root 'users#index'
 end
